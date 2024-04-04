30955d44c30132b1a663ae6d84e93f178ea3d58a
Henry Bennett <henry.bennett@mymail.champlain.edu>
2024-04-04 10:59:42 -0400

    starting skeleton assignment

---
+++ SplinesUnity/Assets/Script/SkeletonScripts/SkeletonBase.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeletonBase : MonoBehaviour
+{
+    //this should be the thing that tracks all the nodes and how I interact with each joint
+}
